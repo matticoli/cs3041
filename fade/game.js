afcaa779dc8e028cf975aa58fca3e458fcae808b
Mikel Matticoli <mamatticoli@wpi.edu>
2018-04-03 10:03:48 -0400

    working

--- fade/game.js
+++ fade/game.js
@@ -19,42 +19,68 @@ Called once after engine is initialized but before event-polling begins.
 
 var tarR, tarG, tarB, tarHex;
 
-PS.init= function(system, options){
-    var x=0, y=0;
+function fill(x, y, r, g, b) {
+    if (x == 0 && y == 0) {
+        PS.applyRect(0, 0, 1, 1, function (i, j) {
+            PS.color(i, j, r, g, b);
+        });
+    }
+
+    if (x == 1 && y == 0) {
+        PS.applyRect(2, 0, 3, 1, function (i, j) {
+            PS.color(i, j, r, g, b);
+        });
+    }
+
+    if (x == 0 && y == 1) {
+        PS.applyRect(0, 2, 1, 3, function (i, j) {
+            PS.color(i, j, r, g, b);
+        });
+    }
+
+    if (x == 1 && y == 1) {
+        PS.applyRect(2, 2, 3, 3, function (i, j) {
+            PS.color(i, j, r, g, b);
+        });
+    }
+}
+
+PS.init = function (system, options) {
+    var x = 0, y = 0;
     var r, g, b;
 
     //set grid to 9 by 9
-    PS.gridSize(9,9);
+    PS.gridSize(8, 8);
 
     tarR = PS.random(256) - 1; // random red 0-255
     tarG = PS.random(256) - 1; // random green
     tarB = PS.random(256) - 1; // random blue
 
-    tarHex="0x" + ((1 << 24) + (tarR << 16) + (tarG << 8) + tarB).toString(16).slice(1)+" ";
+    tarHex = "0x" + ((1 << 24) + (tarR << 16) + (tarG << 8) + tarB).toString(16).slice(1) + " ";
 
 
     PS.statusText("Target: " + tarHex);
 
     //randomize bead colors for entire grid
-    while(y < 9) {
-        while(x < 9) {
+    while (y < 2) {
+        while (x < 2) {
             r = PS.random(256) - 1; // random red 0-255
             g = PS.random(256) - 1; // random green
             b = PS.random(256) - 1; // random blue
 
             //if bead is randomly set to the target hex, reset
-            while(tarR===r && tarG===g && tarB===b) {
+            while (tarR === r && tarG === g && tarB === b) {
                 r = PS.random(256) - 1; // random red 0-255
                 g = PS.random(256) - 1; // random green
                 b = PS.random(256) - 1; // random blue
             }
 
-            PS.color( x, y, r, g, b ); // set bead color
+            fill(x, y, r, g, b); // set bead color
 
-            x= x+1; //increment
+            x = x + 1; //increment
         }
-        y=y+1; //increment
-        x=0; //reset
+        y = y + 1; //increment
+        x = 0; //reset
     }
 };
 
@@ -68,26 +94,62 @@ It doesn't have to do anything.
 [options] = an object with optional parameters; see API documentation for details.
 */
 
-//When the bead is clicked
-PS.touch = function( x, y, data, options ) {
-    var changeXL, changeXR, changeYL, changeYR;
+function spiral(n) {
 
-	PS.audioPlay("fx_click");
+    var k = Math.ceil((Math.sqrt(n) - 1) / 2);
+    var t = 2 * k + 1;
+    var m = t ^ 2;
+    t = t - 1;
 
-	//loop through to change surrounding RGB values
-    changeXL= x-1;
-    changeYL= y-1;
-    changeXR= x+1;
-    changeYR= y+1;
-
-    //HAS TO LOOP WHEN IT MAXES OUT
-    while(changeXL>=0){
-        
-
-       changeXL=changeXL-1;
+    if (n >= m - t) {
+        return [k - (m - n), -k];
+    } else {
+        m = m - t;
     }
 
+    if (n >= m - t) {
+        return [-k, -k + (m - n)];
+    } else {
+        m = m - t
+    }
 
+    if (n >= m - t) {
+        return [-k + (m - n), k];
+    } else {
+        return [k, k - (m - n - t)];
+    }
+}
+
+//When the bead is clicked
+PS.touch = function (x, y, data, options) {
+    // var changeXL, changeXR, changeYL, changeYR;
+    //
+    // PS.audioPlay("fx_click");
+    //
+    // //loop through to change surrounding RGB values
+    // changeXL= x-1;
+    // changeYL= y-1;
+    // changeXR= x+1;
+    // changeYR= y+1;
+    //
+    // //HAS TO LOOP WHEN IT MAXES OUT
+    // while(changeXL>=0){
+    //
+    //
+    //    changeXL=changeXL-1;
+    // }
+
+    for (var i = 0; i < 81; i++) {
+        var coords = spiral(i);
+        var x1 = x + coords[0];
+        var y1 = y + coords[1];
+        console.log(i + ":" + coords + "\t" + (x1 >= 0 && x1 < 9 && y1 >= 0 && y1 < 9));
+        if (x1 >= 0 && x1 < 9 && y1 >= 0 && y1 < 9) {
+            var currentColor = PS.unmakeRGB(PS.color(x1, y1), {});
+            PS.color(x1, y1, 0x0000FF);
+            console.log("AAHH:" + coords);
+        }
+    }
 };
 
 
@@ -102,20 +164,19 @@ It doesn't have to do anything.
 */
 
 //When mouse cursor hovers over a bead
-PS.enter = function( x, y, data, options ) {
+PS.enter = function (x, y, data, options) {
     var color;
     var components;
 
     //find RGB values of current bead
-    components = PS.unmakeRGB( PS.color( x , y ), {} );
+    components = PS.unmakeRGB(PS.color(x, y), {});
 
     //convert RGB values to hex
-    color="0x" + ((1 << 24) + (components.r << 16) + (components.g << 8) + components.b).toString(16).slice(1)+ "\n";
+    color = "0x" + ((1 << 24) + (components.r << 16) + (components.g << 8) + components.b).toString(16).slice(1) + "\n";
 
     //print target hex and current bead's hex
     PS.statusText("Target hex: " + tarHex + "Current: " + color);
 };
-
 
 
 /*
@@ -145,7 +206,6 @@ Called when the mouse cursor/touch exits the grid perimeter.
 It doesn't have to do anything.
 [options] = an object with optional parameters; see API documentation for details.
 */
-
 
 
 /*
